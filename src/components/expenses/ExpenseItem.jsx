a031847915e287d866e7b3102d686348393bbfa3
sdj3959 <dj9099@naver.com>
2025-08-25 12:49:35 +0900

    2-2-1. useState기초

--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css';
-import ExpenseDate from "./ExpenseDate.jsx";
+import ExpenseDate from './ExpenseDate.jsx';
 
 const ExpenseItem = ({expense}) => {
 
-  const { title, price, date } = expense;
+  // 상태변수를 사용하는 useState 훅
+  /*
+    useState훅의 리턴값은 배열이며
+    첫번째 요소는 관리할 상태값의 초기값
+    두번째 요소는 해당 상태값을 변경할 때 사용하는 setter함수
+  */
+  const {title, date, price} = expense;
+
+  const x = useState(title);
+  console.log('x:', x);
+
+
+
+  // 원화 표기법으로 변환
+  const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
 
   // 이벤트 핸들러
-  const clickHandler = (e) => {
-    alert('click!!');
+  const clickHandler = e => {
+    console.log(`변경 전: ${title}`);
+    // title = "짜장면";
+    console.log(`변경 후: ${title}`);
   };
 
   return (
-    <div className='expense-item'>
+    <div className="expense-item">
 
       <ExpenseDate expenseDate={date} />
 
-      <div className='expense-item__description'>
+      <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className='expense-item__price'>{price}원</div>
+        <div className="expense-item__price">{formatPrice}원</div>
       </div>
 
-      <button id={'btn1'} onClick={clickHandler}>버튼1</button>
+      <button id='btn1' onClick={clickHandler}>버튼1</button>
     </div>
   );
 };
