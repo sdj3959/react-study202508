2cbe8063143ce9990bc9faec17f50e05e87094ac
sdj3959 <dj9099@naver.com>
2025-08-25 11:40:33 +0900

    1-4-5. props로 배열 전달하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import ExpenseItem from './components/expenses/ExpenseItem.jsx';
-import ExpenseList from "./components/expenses/ExpenseList.jsx";
+import ExpenseList from './components/expenses/ExpenseList.jsx';
 
-const App = () => (
-  <>
-    <ExpenseList />
-  </>
-);
+const App = () => {
+  const expenseList = [
+    {
+      title: '닭강정',
+      price: 8000,
+      date: new Date(2025, 7, 13)
+    },
+    {
+      title: '호두정과',
+      price: 50000,
+      date: new Date(2025, 8, 21)
+    },
+    {
+      title: '리팩토링',
+      price: 30000,
+      date: new Date(2025, 4, 2)
+    },
+  ];
+
+  return (
+    <>
+      <ExpenseList expenses={expenseList} />
+    </>
+  );
+};
 
 export default App;
--- src/components/expenses/ExpenseList.jsx
+++ src/components/expenses/ExpenseList.jsx
@@ -2,25 +2,7 @@ import React from 'react';
 import './ExpenseList.css';
 import ExpenseItem from "./ExpenseItem.jsx";
 
-const ExpenseList = () => {
-
-  const expenseList = [
-    {
-      title: '닭강정',
-      price: 8000,
-      date: new Date(2025, 7, 13)
-    },
-    {
-      title: '호두정과',
-      price: 5000,
-      date: new Date(2025, 8, 21)
-    },
-    {
-      title: '이펙티브 자바',
-      price: 30000,
-      date: new Date(2025, 4, 2)
-    },
-  ];
+const ExpenseList = ({expenses : expenseList}) => {
 
   return (
     <div className='expenses'>
