e67adcdbef6151457a2a56c8e381b956bf16aa74
sdj3959 <dj9099@naver.com>
2025-08-25 12:32:43 +0900

    2-1-1. 이벤트 바인딩

--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -6,6 +6,11 @@ const ExpenseItem = ({expense}) => {
 
   const { title, price, date } = expense;
 
+  // 이벤트 핸들러
+  const clickHandler = (e) => {
+    alert('click!!');
+  };
+
   return (
     <div className='expense-item'>
 
@@ -15,6 +20,8 @@ const ExpenseItem = ({expense}) => {
         <h2>{title}</h2>
         <div className='expense-item__price'>{price}원</div>
       </div>
+
+      <button id={'btn1'} onClick={clickHandler}>버튼1</button>
     </div>
   );
 };
