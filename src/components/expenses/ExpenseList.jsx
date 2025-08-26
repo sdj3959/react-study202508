e85963aec47c898549e83fb25453faba05c93d04
sdj3959 <dj9099@naver.com>
2025-08-26 10:39:41 +0900

    2-4-1. 상향식 데이터 전달 문제

--- src/components/expenses/ExpenseList.jsx
+++ src/components/expenses/ExpenseList.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import './ExpenseList.css';
 import ExpenseItem from "./ExpenseItem.jsx";
 import Card from "../ui/Card.jsx";
+import ExpenseFilter from "./ExpenseFilter.jsx";
 
 const ExpenseList = ({expenses : expenseList}) => {
 
+  /*
+    ExpenseFilter에서 선택한 연도 값을
+    이 컴포넌트 ExpenseList로 끌어올려서 콘솔에 출력해보세요.
+   */
+  const onFilterChange = (filteredYear) => {
+    console.log(`선택된 연도: ${filteredYear}`);
+  };
+
   return (
     <Card className='expenses'>
-      <ExpenseItem expense={expenseList[0]}/>
-      <ExpenseItem expense={expenseList[1]}/>
-      <ExpenseItem expense={expenseList[2]}/>
+      <ExpenseFilter onChangeFilter={onFilterChange} />
+
     </Card>
   );
 };
