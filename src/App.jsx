2f8b12721c1e3ccb74f4aca377608276db8123ea
sdj3959 <dj9099@naver.com>
2025-08-26 14:09:41 +0900

    실습 1-1. 목표설정 앱 템플릿

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,31 @@
-import React, {useState} from 'react';
-import ExpenseList from './components/expenses/ExpenseList.jsx';
-import NewExpense from './components/new-expense/NewExpense.jsx';
+import React from 'react';
+import CourseList from "./components/CourseGoals/CourseList.jsx";
+import CourseInput from "./components/CourseGoals/CourseInput.jsx";
+import './App.css';
 
 const App = () => {
-  const expenseList = [
+
+  // 목표데이터들의 묶음배열
+  const [goals, setGoals] = useState([
     {
-      title: '닭강정',
-      price: 8000,
-      date: new Date(2025, 7, 13)
+      id: 'g1',
+      text: '테스트 데이터1',
     },
     {
-      title: '호두정과',
-      price: 50000,
-      date: new Date(2025, 8, 21)
+      id: 'g2',
+      text: '테스트 데이터2',
     },
-    {
-      title: '리팩토링',
-      price: 33000,
-      date: new Date(2023, 4, 2)
-    },
-    {
-      title: '에어컨',
-      price: 1000000,
-      date: new Date(2022, 3, 2)
-    },
-  ];
-
-  // 지출항목 배열을 상태관리
-  const [expenses, setExpenses] = useState(expenseList);
-
-  // 상향식 데이터 전달을 위해 하위컴포넌트(ExpenseForm)에게 함수 하나를 내려줌
-  const onAddExpense = (userInput) => {
-    // console.log('상향식 데이터 전달용 함수');
-    // console.log('끌어올려진 데이터: ', userInput);
-
-    // const newArr = [...expenses];
-    // expenses.push(userInput);
-
-    setExpenses(prev => [...prev, userInput]);
-
-    // console.log(expenseList);
-  };
+  ]);
 
   return (
-    <>
-      <NewExpense onSave={onAddExpense} />
-      <ExpenseList expenses={expenses} />
-    </>
+    <div>
+      <section id='goal-form'>
+        <CourseInput />
+      </section>
+      <section id='goals'>
+        <CourseList />
+      </section>
+    </div>
   );
 };
 
