282fdd54023ccab6156c41caf63adb80831fad5d
sdj3959 <dj9099@naver.com>
2025-08-28 11:10:13 +0900

    6-1-1. side effect 템플릿

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import React, {useState} from 'react';
 import './App.css';
 import AddUsers from './components/Users/AddUsers.jsx';
 import UserList from './components/Users/UserList.jsx';
-import MainHeader from "./components/SideEffect/MainHeader.jsx";
+import MainHeader from './components/SideEffect/MainHeader.jsx';
+import Home from './components/SideEffect/Home.jsx';
+import Login from './components/SideEffect/Login.jsx';
 
 const App = () => {
+
 
   return (
     <>
       <MainHeader />
-      <Main>
+      <main>
         <Home />
         <Login />
-      </Main>
+      </main>
     </>
   );
 };
