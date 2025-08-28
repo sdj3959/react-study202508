57ff8394468dcb164dfe9b4671310773edf2d5b3
sdj3959 <dj9099@naver.com>
2025-08-28 14:33:37 +0900

    6장 복습예제. 5-타이머가 중지되지 않는 이유

--- src/components/TimerGame/TimerChallenge.jsx
+++ src/components/TimerGame/TimerChallenge.jsx
@@ -8,15 +8,36 @@ const TimerChallenge = ({ title, targetTime }) => {
   // 타이머가 만료되었는지 확인하는 상태값
   const [timerExpired, setTimerExpired] = useState(false);
 
+  // 타이머 id를 저장
+  let timerId;
+
+  // 타이머를 시작하는 이벤트
   const hanldeStart = e => {
+    console.log('타이머가 시작됨!');
     setTimerStarted(true);
 
     // 실제 정해진 시간만큼 타이머를 가동
-    setTimeout(() => {
+    timerId = setTimeout(() => {
       console.log(targetTime + 's 타이머 만료!');
       setTimerExpired(true);
     }, targetTime * 1000);
-  }
+  };
+
+  /*
+      setTimeout실행시 타이머의 id(aaa)가 생성된다.
+      타이머 실행시 setTimerExpired가 실행됨.
+
+      상태변수는 setter 실행시 컴포넌트를 리렌더링함.
+      리렌더링할 때 기존에 저장한 timer id(aaa)를 없앤다.
+
+      따라서 제대로 clear를 수행하지 못함
+    */
+
+  // 타이머를 중단하는 함수
+  const handleStop = e => {
+    console.log('타이머가 중지됨! timerId - ', timerId);
+    clearTimeout(timerId);
+  };
 
   return (
     <section className='challenge'>
@@ -26,7 +47,7 @@ const TimerChallenge = ({ title, targetTime }) => {
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button onClick={hanldeStart}>
+        <button onClick={timerStarted ? handleStop : hanldeStart}>
           {timerStarted ? 'Stop' : 'Start'} Challenge
         </button>
       </p>
