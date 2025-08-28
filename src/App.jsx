b02d02a1503a64dfea801ccff11672369d14fbb6
sdj3959 <dj9099@naver.com>
2025-08-28 10:04:27 +0900

    5-2-2. 사용자 추가와 목록 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,24 @@
 import React, {useState} from 'react';
 import './App.css';
-import TodoTemplate from "./components/Todos/TodoTemplate.jsx";
+import AddUsers from './components/Users/AddUsers.jsx';
+import UserList from './components/Users/UserList.jsx';
 
 const App = () => {
 
-  const [ todos, setTodos ] = useState([
-    { id: Math.random().toString(), text: '리액트 공부하기', done: false },
-    { id: Math.random().toString(), text: '컴포넌트 스타일링 해보기', done: false },
-    { id: Math.random().toString(), text: '일정 관리 앱 만들어 보기', done: false },
-  ]);
+  // 회원들이 저장될 배열
+  const [userList, setUserList] = useState([]);
 
-  const onAddTodo = (text) => {
-    // 새로운 할 일 추가 로직
-    setTodos(prev => [
-        ...prev
-        , { id: Math.random().toString(), text, done: false }
-      ]);
-
-    return todos;
+  // 입력한 회원정보를 가져오는 함수
+  const addUserInfo = (user) => {
+    setUserList([...userList, user]);
   };
 
-  const onDeleteTodo = (id) => {
-    // 할 일 삭제 로직
-    setTodos(prev => prev.filter(todo => todo.id !== id));
-    return todos;
-  };
-
-  // done이 false인 항목의 개수
-  const todoCount = todos.filter(todo => !todo.done).length;
-
-  const onCheckTodo = (id) => {
-    setTodos(prev => prev.map(todo => todo.id === id ? { ...todo, done: !todo.done } : todo));
-    return todos;
-  };
-
-  return <TodoTemplate onAddTodo={onAddTodo} todoList={todos} onDeleteTodo={onDeleteTodo} todoCount={todoCount} onCheckTodo={onCheckTodo} />
+  return (
+    <>
+      <AddUsers onAddUser={addUserInfo} />
+      <UserList users={userList} />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Users/AddUsers.jsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import styles from './AddUsers.module.css';
+import Card from '../ui/Card';
+import Button from '../ui/Button';
+
+const AddUsers = ({ onAddUser }) => {
+
+  // 입력값들을 상태관리
+  const [userValue, setUserValue] = useState({
+    username: '',
+    age: '',
+  });
+
+  const handleName = (e) => {
+    setUserValue({
+      ...userValue,
+      username: e.target.value,
+    });
+  };
+  const handleAge = (e) => {
+    setUserValue({
+      ...userValue,
+      age: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onAddUser({
+      ...userValue,
+      id: Math.random().toString(),
+    });
+
+    setUserValue({
+      username: '',
+      age: '',
+    });
+  };
+
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>이름</label>
+        <input
+          id='username'
+          type='text'
+          value={userValue.username}
+          onInput={handleName}
+        />
+        <label htmlFor='age'>나이</label>
+        <input
+          id='age'
+          type='number'
+          value={userValue.age}
+          onInput={handleAge}
+        />
+        <Button type='submit'>가입하기</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUsers;
