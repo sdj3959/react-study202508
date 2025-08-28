274648ee1d48cc766e9785261353715a60ce8a86
sdj3959 <dj9099@naver.com>
2025-08-28 11:04:55 +0900

    5-2-7. useRef

--- src/components/Users/AddUsers.jsx
+++ src/components/Users/AddUsers.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import styles from './AddUsers.module.css';
 import Card from '../ui/Card';
 import Button from '../ui/Button';
@@ -7,30 +7,43 @@ import ErrorModal from "../ui/Modal/ErrorModal.jsx";
 const AddUsers = ({ onAddUser }) => {
 
   // 입력값들을 상태관리
-  const [userValue, setUserValue] = useState({
-    username: '',
-    age: '',
-  });
+  // const [userValue, setUserValue] = useState({
+  //   username: '',
+  //   age: '',
+  // });
+
+  // useRef로 태그 기억시키기
+  const usernameRef = useRef();
+  const ageRef = useRef();
 
   // 에러가 났을 때 에러데이터를 관리할 상태변수
   // error -> {title: 에러제목, message: 에러원인}
   const [error, setError] = useState(null);
 
-  const handleName = (e) => {
-    setUserValue({
-      ...userValue,
-      username: e.target.value,
-    });
-  };
-  const handleAge = (e) => {
-    setUserValue({
-      ...userValue,
-      age: e.target.value,
-    });
-  };
+  // const handleName = (e) => {
+  //   setUserValue({
+  //     ...userValue,
+  //     username: e.target.value,
+  //   });
+  // };
+  // const handleAge = (e) => {
+  //   setUserValue({
+  //     ...userValue,
+  //     age: e.target.value,
+  //   });
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // input태그 꺼내기
+    const $usernameInput = usernameRef.current;
+    const $ageInput = ageRef.current;
+
+    const userValue = {
+      username: $usernameInput.value,
+      age: $ageInput.value,
+    }
 
     // 입력값 검증
     if (!userValue.username.trim()) {
@@ -55,10 +68,15 @@ const AddUsers = ({ onAddUser }) => {
       id: Math.random().toString(),
     });
 
-    setUserValue({
-      username: '',
-      age: '',
-    });
+    // setUserValue({
+    //   username: '',
+    //   age: '',
+    // });
+
+    $usernameInput.value = '';
+    $ageInput.value = '';
+
+    $usernameInput.focus();
   };
 
 
@@ -71,15 +89,17 @@ const AddUsers = ({ onAddUser }) => {
           <input
             id='username'
             type='text'
-            value={userValue.username}
-            onInput={handleName}
+            // value={userValue.username}
+            // onInput={handleName}
+            ref={usernameRef}
           />
           <label htmlFor='age'>나이</label>
           <input
             id='age'
             type='number'
-            value={userValue.age}
-            onInput={handleAge}
+            ref={ageRef}
+            // value={userValue.age}
+            // onInput={handleAge}
           />
           <Button type='submit'>가입하기</Button>
         </form>
