29e9b0402d981c3e1f03595e46f0c6cbb43f8e25
sdj3959 <dj9099@naver.com>
2025-08-28 09:16:37 +0900

    TODO 실습 더미데이터 js 파일 추가

---
+++ src/components/Todos/dummy-data/DUMMY_TODOS.js
@@ -0,0 +1,6 @@
+export const TODOS = [
+  { id: 1, text: '리액트 학습', done: false },
+  { id: 2, text: '점심 먹기', done: true },
+  { id: 3, text: '프로젝트 연구하기', done: true },
+  { id: 4, text: '숙제하기', done: false },
+];
